39d9c842a92edc0ab2a5421fdbdc9d1814c3f951
Leo Moraes <moraes.leo@gmail.com>
2019-09-23 21:55:31 -0300

    07 - Adicionando repositórios - PARTE 1 Converter ../Main/index.js em Componente de classe Testando o botão de submit

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { FaGithubAlt, FaPlus } from "react-icons/fa"; //pacote com fontes de icones Font-Awesome... Icones
 
 import { Container, Form, SubmitButton } from "./styles";
 
-export default function Main() {
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+  };
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.newRepo);
+  };
+
+  render() {
+    const { newRepo } = this.state;
+
   return (
     <Container>
       <h1>
@@ -12,16 +28,19 @@ export default function Main() {
         Repositórios
       </h1>
 
-      <Form onSubmit={() => {}}>
+        <Form onSubmit={this.handleSubmit}>
         <input
           type="text"
           placeholder= "Adicionar repositório"
+            value={ newRepo }
+            onChange={ this.handleInputChange }
         />
 
-        <SubmitButton disabled>
+          <SubmitButton >
             <FaPlus color="#FFF" size={14} />
         </SubmitButton>
       </Form>
     </Container>
-    )
+      );
+  }
 }
