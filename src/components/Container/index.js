245bf33347c976f4ed9f6b5e285edaeaf41ec8eb
Leo Moraes <moraes.leo@gmail.com>
2019-09-26 19:54:13 -0300

    14 - Exibindo issues https://github.com/leohmoraes/rocketseat-05/issues/14

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 // Nao é possivel colocar export default em uma constante
 
 const Container = styled.div`
-  max-width: 450px; /* largura maxima, no resize da janela */
+  max-width: 700px; /* largura maxima, no resize da janela */
   background: #FFF;
   border-radius: 4px;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.1); /* sombra leve no lado de fora do box */
